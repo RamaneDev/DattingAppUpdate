15d747833ea37424fbf0d945c6647ea973f8f1cf
mesri <abde_mes@yahoo.fr>
2023-03-28 14:52:58 +0200

    [Client] navbar component

--- DatingAngluarClient/src/app/app.component.ts
+++ DatingAngluarClient/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'DatingClient';
+export class AppComponent implements OnInit {
+  users: any;
+  
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5002/api/Users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    });
+  }
+  
+
+  
 }
