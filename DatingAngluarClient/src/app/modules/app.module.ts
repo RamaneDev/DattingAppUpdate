bab2562d160a064e42bb693ab9f0c0c1c22bdd39
mesri <abde_mes@yahoo.fr>
2023-04-01 15:32:49 +0200

    [Client]creating some components and impl routing funtionality

--- DatingAngluarClient/src/app/modules/app.module.ts
+++ DatingAngluarClient/src/app/modules/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+import { AppComponent } from '../app.component';
+import { NavComponent } from '../nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from '../home/home.component';
+import { RegisterComponent } from '../register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { ListsComponent } from '../lists/lists.component';
+import { MemberDetailComponent } from '../members/member-detail/member-detail.component';
+import { MemberListComponent } from '../members/member-list/member-list.component';
+import { MessagesComponent } from '../messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -17,11 +22,16 @@ import { ToastrModule } from 'ngx-toastr';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    ListsComponent,
+    MemberDetailComponent,
+    MemberListComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    AppRoutingModule,
     FontAwesomeModule,
     BrowserAnimationsModule,    
     ToastrModule.forRoot({
