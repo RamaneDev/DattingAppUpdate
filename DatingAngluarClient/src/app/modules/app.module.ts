6bb6a94566ce249c5198abd91791315fcf55afa8
mesri <abde_mes@yahoo.fr>
2023-04-06 03:52:17 +0200

    [Client] updating app.module & scss of navbar

--- DatingAngluarClient/src/app/modules/app.module.ts
+++ DatingAngluarClient/src/app/modules/app.module.ts
@@ -16,6 +16,8 @@ import { NotFoundComponent } from '../errors/not-found/not-found.component';
 import { ServerErrorComponent } from '../errors/server-error/server-error.component';
 import { TestErrorsComponent } from '../errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from '../interceptors/error.interceptor';
+import { MemberCardComponent } from '../members/member-card/member-card.component';
+import { JwtInterceptor } from '../interceptors/jwt.interceptor';
 
 
 @NgModule({
@@ -30,7 +32,8 @@ import { ErrorInterceptor } from '../interceptors/error.interceptor';
     MessagesComponent,
     NotFoundComponent,
     ServerErrorComponent,
-    TestErrorsComponent
+    TestErrorsComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +43,8 @@ import { ErrorInterceptor } from '../interceptors/error.interceptor';
     SharedModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
