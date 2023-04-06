3877fcaaeef9bcd8c7f0c73b87e6027bc260d2d5
mesri <abde_mes@yahoo.fr>
2023-04-06 03:51:26 +0200

    [Client] member-list component

--- DatingAngluarClient/src/app/members/member-list/member-list.component.ts
+++ DatingAngluarClient/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Members } from 'src/app/models/members';
+import { MembersService } from 'src/app/services/members.service';
 
 @Component({
   selector: 'app-member-list',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberListComponent implements OnInit {
 
-  constructor() { }
+  members: Members[] = [];
+
+  constructor(private membersService: MembersService) { }
 
   ngOnInit(): void {
+    this.getMembers();
+    this.getMember();
+  }
+
+  getMembers() {
+    this.membersService.getMembers().subscribe({
+      next: resp => this.members = resp,
+      error : err => console.log(err) 
+    });
+  }
+
+  getMember() {
+    this.membersService.getMember('Lola').subscribe({
+      next: resp => console.log(resp),
+      error : err => console.log(err) 
+    });
   }
 
 }
