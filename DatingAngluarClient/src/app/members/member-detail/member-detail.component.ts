a7b507caba6cf0e5fa8a7d0db8fc2883b0d69514
mesri <abde_mes@yahoo.fr>
2023-04-06 05:14:45 +0200

    [Client] fetching member informations for member-detail comp

--- DatingAngluarClient/src/app/members/member-detail/member-detail.component.ts
+++ DatingAngluarClient/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/models/member';
+import { MembersService } from 'src/app/services/members.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.scss']
 })
 export class MemberDetailComponent implements OnInit {
-
-  constructor() { }
+  member: Member | undefined;
+  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getMember();
+  }
+
+  getMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+    if(username) {
+      this.memberService.getMember(username).subscribe({
+        next: user => this.member = user
+      });
+    } 
+    
   }
 
 }
