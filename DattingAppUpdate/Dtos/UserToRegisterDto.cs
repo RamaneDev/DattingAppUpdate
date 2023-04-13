987976fbbb3d4591318a2669315ac007bcdb4f04
mesri <abde_mes@yahoo.fr>
2023-04-13 18:47:37 +0200

    [API] extend UserToRegisterDto

--- DattingAppUpdate/Dtos/UserToRegisterDto.cs
+++ DattingAppUpdate/Dtos/UserToRegisterDto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DattingAppUpdate.Dtos
 {
@@ -6,7 +7,22 @@ namespace DattingAppUpdate.Dtos
     {
         [Required]
         public string Username { get; set; }
-
+        
+        [Required]
+        public string KnowsAs { get; set; }
+        
+        [Required]
+        public string Gender { get; set; }
+        
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        
+        [Required]
+        public string City { get; set; }
+        
+        [Required]
+        public string Country { get; set; }
+        
         [Required]
         [StringLength(15, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 15 caracteres")]
         public string Password { get; set; }
