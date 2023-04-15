0e30695426ba751cc8d09e9edff0de9130654815
mesri <abde_mes@yahoo.fr>
2023-04-15 04:15:52 +0200

    [Client] date-input & text-input component

---
+++ DatingAngluarClient/src/app/forms/text-input/text-input.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, Self } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { NgControl } from '@angular/forms';
+import { ControlValueAccessor } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.scss']
+})
+export class TextInputComponent implements ControlValueAccessor {
+
+  @Input() label: string = '';
+  @Input() type = 'text';
+  
+  constructor(@Self() public ngControl: NgControl) { 
+    this.ngControl.valueAccessor = this;
+  }
+  
+  
+  writeValue(obj: any): void {    
+  }
+  registerOnChange(fn: any): void {   
+  }
+  registerOnTouched(fn: any): void {    
+  }
+  setDisabledState?(isDisabled: boolean): void {   
+  }
+
+  ngOnInit(): void {
+  }
+
+  get control(): FormControl {
+    return this.ngControl.control as FormControl;
+  }
+
+}
