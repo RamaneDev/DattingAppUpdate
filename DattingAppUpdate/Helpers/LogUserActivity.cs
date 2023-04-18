43a3b251287683e00e547d0af9c1e597fa2e7b59
mesri <abde_mes@yahoo.fr>
2023-04-18 02:52:36 +0200

    [API] updating lastActive of user using actionFilter

---
+++ DattingAppUpdate/Helpers/LogUserActivity.cs
@@ -0,0 +1,25 @@
+﻿using DattingAppUpdate.Extensions;
+using DattingAppUpdate.IService;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace DattingAppUpdate.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+
+            if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
+
+            var userId = resultContext.HttpContext.User.GetUserId();
+            var repo = resultContext.HttpContext.RequestServices.GetService<IDatingRepository>();
+            var user = await repo.GetUserToUpdateByIdAsync(userId);
+            user.LastActive = DateTime.Now;
+            await repo.SaveAllAsync();
+        }
+    }
+}
