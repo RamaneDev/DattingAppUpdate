cb0ae7e9ea85fd4748527204e7dd1001cd2fd539
mesri <abde_mes@yahoo.fr>
2023-04-18 02:55:32 +0200

    [API] PaginationHeader to return to client for each request

--- DattingAppUpdate/Extensions/Extensions.cs
+++ DattingAppUpdate/Extensions/Extensions.cs
@@ -1,8 +1,11 @@
 ﻿using DattingAppUpdate.Entites;
+using DattingAppUpdate.Helpers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Security.Claims;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace DattingAppUpdate.Extensions
@@ -33,6 +36,24 @@ namespace DattingAppUpdate.Extensions
         public static string GetUsername(this ClaimsPrincipal user)
         {
             return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+        }
+
+        public static void AddPaginationHeader(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader, options));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
         }
     }
 }
